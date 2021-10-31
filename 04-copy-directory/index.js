ccdf68415b59fdc5125b83fd8c719dc368f46b77
zhulym <zhulym@mail.ru>
2021-10-31 14:01:59 +0300

    feat: finish task 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,36 @@
+const fs = require('fs');
+const promise = require('fs').promises;
+const path = require('path');
+const { stdout } = process;
+const sym = '\u2550';
 
+const distFiles = path.join(__dirname, 'files');
+const distFilesCopy = path.join(__dirname, 'files-copy');
+
+fs.readdir(distFilesCopy, (error, files) => {
+  if (!error) {
+    for (const file of files) {
+      fs.unlink(path.join(distFilesCopy, file), error => {
+        if (error) throw error;
+      });
+    }
+  } else return;
+});
+
+fs.mkdir(distFilesCopy, { recursive: true }, error => {
+  if (error) throw error;
+});
+
+fs.readdir(distFiles, { withFileTypes: true }, (error, files) => {
+  if (error) throw error;
+  for (let file of files) {
+    const currentFile = file.name.toString();
+    stdout.write(`File ${file.name} completed!\n`);
+    promise.copyFile(path.join(__dirname, 'files', currentFile), path.join(__dirname, 'files-copy', currentFile))
+      .catch(error => {
+        if (error) throw error;
+      })
+  }
+})
+
+stdout.write(`${sym.repeat(60)}\n`);
